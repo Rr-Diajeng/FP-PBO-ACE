e99599fa15233f5855799f5536eeaec7ad7afca4
Rr-Diajeng <91377782+Rr-Diajeng@users.noreply.github.com>
2022-12-01 07:10:55 +0700

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,8 +1,5 @@
 package Com.Game;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Random;
@@ -12,81 +9,85 @@ import java.util.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+
 public class Game extends JPanel implements KeyListener{
+
 	
 	private Board game = new Board();
 	private static Game newGame = new Game();
 	private Color c;
 	
-	static JFrame frame = new JFrame("2048");;
+	static JFrame frame = new JFrame("2048");
 	
+
 	public static void setUpGUI() {
 			
 		frame = new JFrame("2048");
 		
 		frame.addKeyListener(newGame);
 		frame.getContentPane().add(newGame);
-		frame.setSize(600, 400);
+		frame.setSize(1280, 700);
 		frame.setVisible(true);
-		frame.setResizable(false);
-
+		frame.setResizable(true);
+		
 	}
 	
 	public void paint(Graphics g) {
 		
 		super.paint(g);
-		g.drawString("2048", 250, 20);
-		g.drawString( "Score: " + game.getScore(), 200 - 4 * String.valueOf( game.getScore() ).length(), 40);
-		g.drawString("Highest Tile: " + game.getHighTile(), 280 - 4 * String.valueOf(game.getHighTile()).length(), 40);
-		g.drawString("Press 'Enter' to Start", 210, 315);
-		g.drawString("Use 'wasd' or Arrow Keys to move", 180, 335);
+		g.drawString("2048", 640, 20);
+		g.drawString( "Score: " + game.getScore(), 400 - 4 * String.valueOf( game.getScore() ).length(), 70);
+		g.drawString("Highest Tile: " + game.getHighTile(), 880 - 4 * String.valueOf(game.getHighTile()).length(), 70);
+		g.drawString("Use 'wasd' or Arrow Keys to move", 570, 600);
 		
-		if(game.blackOut()) g.drawString("Press 'Enter' to restart", 200, 355);
+		if(game.blackOut() == true)g.drawString("Press 'Enter' to restart", 610, 575);
+		else g.drawString("Press 'Enter' to Start", 610, 575);
 		
 		g.setColor(Color.gray);
-		g.fillRect(140, 50, 250, 250);
+		g.fillRect(430, 100, 450, 450);
 		
 		for(int i = 0; i < 4; i++) {
 			for(int j = 0; j < 4; j++) {
-				drawTiles(g, game.board[i][j], j * 60 + 150, i * 60 + 60);
+				drawTiles(g, game.board[i][j], j * 100 + 460, i * 100 + 130);
 			}
 		}
 		
 		if(game.gameOver()) {
 			
 			g.setColor(Color.gray);
-			g.fillRect(140, 50, 250, 250);
+			g.fillRect(430, 100, 450, 450);
 			
 			for(int i = 0; i < 4; i++) {
 				for(int j = 0; j < 4; j++) {
 					
 					g.setColor(Color.RED);
-					g.fillRoundRect(j * 60 + 150, i * 60 + 60, 50, 50, 5, 5);
+					g.fillRoundRect(j * 100 + 460, i * 100 + 130, 95, 95, 5, 5);
 					g.setColor(Color.BLACK);
-					g.drawString("GAME", j * 60 + 160, i * 60 + 75);
-					g.drawString("OVER", j * 60 + 160, i * 60 + 95);
+					g.drawString("GAME", j * 100 + 490, i * 100 + 170);
+					g.drawString("OVER", j * 100 + 490, i * 100 + 190);
 				}
 			}
 		}
 	}
 	
-	public void drawTiles(Graphics g, Tile tile, int x, int y) {
+	void drawTiles(Graphics g, Tile tile, int x, int y) {
 		
 		int tileValue = tile.getValue();
 		int length = String.valueOf(tileValue).length();
 		
 		g.setColor(Color.lightGray);
-		g.fillRoundRect(x, y, 50, 50, 5, 5);
+		g.fillRoundRect(x, y, 95, 95, 5, 5); // besar kotak 95 
 		g.setColor(Color.BLACK);
 		
 		if(tileValue > 0) {
 			
 			g.setColor(tile.getColor());
-			g.fillRoundRect(x, y, 50, 50, 5, 5);
+			g.fillRoundRect(x, y, 95, 95, 5, 5);
 			g.setColor(Color.BLACK);
-			g.drawString("" + tileValue, x + 25 - 3 * length, y + 25);
+			g.drawString("" + tileValue, x + 48 - 3 * length, y + 49);
 		}
 	}
+	
 	
 	
 	@Override
