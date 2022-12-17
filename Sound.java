eb44665926aa3dcaf1d6cf1c18cd52868902066d
Ra <91133817+Dancingaroundthelies@users.noreply.github.com>
2022-12-17 20:39:18 +0700

    Add files via upload

---
+++ Sound.java
@@ -0,0 +1,42 @@
+package Com.Game;
+
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound {
+	
+	Clip clip;
+	URL soundURL[] = new URL[30];
+	
+	public Sound() {
+		soundURL[1] = getClass().getResource("/Music/GameOver.wav");
+		soundURL[2] = getClass().getResource("/Music/fun.wav");
+	}
+	
+	public void setFile(int i) {
+		
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+			}catch(Exception e) {
+				
+			}	
+	}
+	
+	public void play() {
+		clip.start();
+	}
+	
+	public void loop() {
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+	}
+	
+	public void stop() {
+		clip.stop();
+	}
+
+}
